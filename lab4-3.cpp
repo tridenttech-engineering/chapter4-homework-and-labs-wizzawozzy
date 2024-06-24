c14d10705474cf6149a88f485a201eea571bcd17
Matthew <matthewcollinsece@gmail.com>
2024-06-24 12:24:24 -0400

    Updates for students

--- lab4-3.cpp
+++ lab4-3.cpp
@@ -6,6 +6,20 @@ using namespace std;
 
 int main()
 {
+  //declare named constant and variables
+  const double COMM_RATE = 0.1;
+  double sales = 0.0;
+  double commission = 0.0;
+
+  //enter input item
+  cout << "Sales amount: ";
+  cin >> sales;
+
+  //calculate and display the commision
+  commission = sales * COMM_RATE;
+  cout << "Commission: $" << commission << endl;
+
+  return 0;
 
   return 0;
 } //end of main function
